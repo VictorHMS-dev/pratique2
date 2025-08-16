22ae6486bd156a317e59aa408352da8fa1ca85ce
VictorHMS-dev <victor.machado.santos63@gmail.com>
2025-08-16 19:26:54 -0300

    correção nome branch

--- main.js
+++ main.js
@@ -53,5 +53,4 @@ document.getElementById("numero-1").addEventListener("input", () => {
 });
 document.getElementById("numero-2").addEventListener("input", () => {
     document.querySelector('.error-message').classList.remove("show");
-
 });
